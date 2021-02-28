06eda72a2176bca88710373f2cbab018714f946a
Yusuf Özgül <yozgul@icloud.com>
2021-02-28 21:07:05 +0300

    Update XibLoadable for auto loadable

--- Sources/YTools-Swift/XibLoadable.swift
+++ Sources/YTools-Swift/XibLoadable.swift
@@ -1,9 +1,34 @@
 #if canImport(UIKit)
 import UIKit
 
-public extension UIView {
-    class func loadXib<T: UIView>() -> T {
-        return Bundle(for: T.self).loadNibNamed(String(describing: T.self), owner: nil, options: nil)?[0] as? T ?? T()
+open class XibLoadable: UIView {
+    @IBOutlet weak var view: UIView!
+
+    override public init(frame: CGRect) {
+        super.init(frame: frame)
+        xibSetup()
+    }
+
+    required public init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        xibSetup()
+    }
+
+    private func xibSetup() {
+        backgroundColor = .clear
+
+        view = loadViewFromNib()
+        view.frame = bounds
+        view.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        view.translatesAutoresizingMaskIntoConstraints = true
+
+        addSubview(view)
+    }
+
+    private func loadViewFromNib() -> UIView {
+        let bundle = Bundle(for: type(of: self))
+        let nib = UINib(nibName: String(describing: type(of: self)), bundle: bundle)
+        return nib.instantiate(withOwner: self, options: nil).first as! UIView
     }
 }
 #endif
